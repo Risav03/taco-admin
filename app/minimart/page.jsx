8e42c4e6acb005201daaaaf5996728e9c68e5285
Risav03 <needleweed03@gmail.com>
2024-02-05 22:34:22 +0530

    minimart done

--- app/minimart/page.jsx
+++ app/minimart/page.jsx
@@ -3,11 +3,242 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import {ethers} from "ethers";
+import erc721abi from '@/utils/erc721abi';
+import Image from "next/image"
+import Swal from"sweetalert2"
+import minimartabi from '@/utils/minimartabi';
 
 const MinmartDashboard = () => {
+
+  const[contractAdd, setContractAdd] = useState("");
+  const[tokenId, setTokenId] = useState(null);
+
+  const[price, setPrice] = useState(null);
+  const [displayNFT, setDisplayNFT] = useState([]);
+  const[loading, setLoading] = useState(false);
+
+
+  const address = "0x7F02956863dA568A83610364B7425AE8418e0dFC"
+
+  async function minimartContractSetup() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    try {
+      const contract = new ethers.Contract(address, minimartabi, signer);
+      return contract;
+    }
+    catch (err) {
+
+
+      console.log("Error", err)
+      Swal.fire({
+        title: 'Error!',
+        text: 'Couldn\'t get fetching contract',
+        imageUrl: error,
+        imageWidth: 200,
+        imageHeight: 200,
+        imageAlt: "Taco OOPS!",
+        confirmButtonText: 'Bruh 😭',
+        confirmButtonColor: "#facc14",
+        customClass: {
+          container: "border-8 border-black",
+          popup: "bg-white rounded-2xl border-8 border-black",
+          image: "-mb-5",
+          confirmButton: "w-40 text-black"
+        }
+      })
+    }
+  }
+  async function setMinimartItem() {
+    try {
+      const contract = await minimartContractSetup();
+
+
+      const txn = await contract.setMinimartItem(contractAdd, tokenId, ethers.utils.parseEther(String(price)));
+      txn.wait().then(() => {
+        setLoading(false);
+        Swal.fire({
+          icon: "success",
+          title: "NFT has been listed for " + ethers.utils.formatEther(String(price)) + " $GUAC",
+          showConfirmButton: false,
+          timer: 1500
+        }).then((res) => { window.location.reload() });
+      });
+
+    }
+    catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  }
+
+  async function setERC721(contractAdd) {
+    try {
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      if (contractAdd.toUpperCase() != "0X0000000000000000000000000000000000000000") {
+        const contract = new ethers.Contract(contractAdd, erc721abi, signer);
+        return contract
+      }
+
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function setERC721Contract(){
+    try{
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+        const contract = new ethers.Contract(contractAdd, erc721abi, signer);
+        return contract
+      }
+
+    
+    catch(err){
+      console.log(err);
+    }
+  }
+
+  async function displayListedNFTs() {
+    try {
+      // const contract = await setERC721Contract();
+      const arr = [];
+      
+      const minimartContract = await minimartContractSetup();
+      
+      const data = await minimartContract.fetchData();
+      
+      for (let i = 0; i < data.length; i++) {
+        
+        const contract = await setERC721(data[i][0]);
+        
+        const contractAdd = data[i][0];
+        
+        if (contractAdd.toUpperCase() != "0X0000000000000000000000000000000000000000") {
+          console.log("helloooooo", contractAdd);
+          const tokenId = String(data[i][1]);
+          const uri = await contract.tokenURI(tokenId);
+          const metadata = "https://ipfs.io/ipfs/" + uri.substr(7);
+          const meta = await fetch(metadata);
+          const json = await meta.json();
+          const name = json["name"];
+          const img = "https://ipfs.io/ipfs/" + json["image"].substr(7);
+          const price = ethers.utils.formatEther(String(data[i][3]));
+          const owner = String(data[i][2]);
+
+          arr.push({ name, tokenId, img, price, owner, i });
+        }
+      }
+
+      setDisplayNFT(arr);
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+
+  async function approval(){
+
+    try {
+      setLoading(true);
+    const contract = await setERC721Contract();
+    const approval = await contract?.approve(address, tokenId);
+
+    approval.wait().then((res)=>{
+      setMinimartItem()
+    });
+
+
+
+    }
+    catch (err) {
+    console.log("Error", err);
+    setLoading(false);
+      Swal.fire({
+        title: 'Error!',
+        text: 'Couldn\'t get fetching contract',
+        imageUrl: error,
+        imageWidth: 200,
+        imageHeight: 200,
+        imageAlt: "Taco OOPS!",
+        confirmButtonText: 'Bruh 😭',
+        confirmButtonColor: "#facc14",
+        customClass: {
+          container: "border-8 border-black",
+          popup: "bg-white rounded-2xl border-8 border-black",
+          image: "-mb-5",
+          confirmButton: "w-40 text-black"
+        }
+      })
+    }
+
+}
+
+  function handleContractAddress(e){
+    setContractAdd(e.target.value);
+}
+
+function handleTokenId(e){
+    setTokenId(e.target.value);
+}
+
+function handlePrice(e){
+  setPrice(e.target.value);
+}
+
+useEffect(()=>{
+  displayListedNFTs()
+},[])
+
   return (
-    <div className='w-[80%] mt-20 mx-auto grid grid-cols-2 gap-5 gap-y-10'>
-       
+    <div className='w-[80%] mt-20 mx-auto'>
+      <div className='mx-auto'>
+
+        <h1 className="font-bold text-4xl mb-10 text-center">Set Item:</h1>
+        <div className='border-4 w-[50%] border-black bg-yellow-400 rounded-2xl p-5 flex flex-col gap-5 mx-auto'>
+          
+          <div>
+            <h1 className='font-bold'>Contract Address:</h1>
+            <input onChange={handleContractAddress} value={contractAdd} type="text" className='px-4 h-12 w-full rounded-lg bg-white text-lg border-2 border-black' />
+          </div>
+
+          <div className='font-bold'>
+            <h1>Token ID:</h1>
+            <input onChange={handleTokenId} value={tokenId} type="number" min={0} className='px-4 h-12 w-full rounded-lg bg-white text-lg border-2 border-black' />
+          </div>
+
+          <div className='font-bold'>
+            <h1>Price:</h1>
+            <input onChange={handlePrice} value={price} type="number" min={0} className='px-4 h-12 w-full rounded-lg bg-white text-lg border-2 border-black' />
+          </div>
+
+          <button onClick={approval} className={`bg-green-500 border-2 border-black hover:bg-green-600 ${loading && "animate-spin"} duration-300 rounded-2xl px-6 py-3 text-3xl mx-auto font-bold`}>List</button>
+      </div>
+
+
+       </div>
+
+       <div className='mt-10'>
+       <h1 className="font-bold text-4xl mb-10 text-center ">Currently Listed:</h1>
+       <div className="flex gap-5 flex-wrap text-center">
+       {displayNFT.map((item) => (
+            <div className="mx-auto">
+              <Image width={1920} height={1080} src={item.img} className="shadow-xl bg-white shadow-black/30 w-52 h-52 mx-auto rounded-2xl relative z-[2] border-2 border-black" />
+              <div className="bg-red-400 w-60 px-5 -translate-y-24 shadow-2xl shadow-black/60 pt-28 border-4 border-black rounded-2xl">
+                <h1 className="text-black text-lg">{item.name}</h1>
+                <h1 className="text-black bg-yellow-400 border-2 py-2 rounded-2xl border-black">{item.price} $GUAC</h1>
+                {address.toLowerCase() === item.owner.toLowerCase() ? <button disabled={loading} onClick={() => { unList(item.i) }} className={`bg-red-500 py-2 text-2xl ${loading && " animate-spin "} px-5 my-3 rounded-2xl border-2 border-black hover:bg-red-600`}>Unlist</button> : <button onClick={() => { approve(item.price, item.i) }} className={`bg-blue-500 relative text-2xl py-2 px-5 my-3 ${loading && " animate-spin "} rounded-2xl border-2 border-black hover:bg-blue-600`}>Buy</button>}
+              </div>
+                </div>
+          ))}
+          </div>
+       </div>
     </div>
   )
 }
---
+++ utils/minimartabi.js
@@ -0,0 +1,315 @@
+export default [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "cost",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "index",
+				"type": "uint256"
+			}
+		],
+		"name": "buyMinimartItem",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "index",
+				"type": "uint256"
+			}
+		],
+		"name": "removeApprovedCollection",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "collection",
+				"type": "address"
+			}
+		],
+		"name": "setApprovedCollection",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "contractAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			}
+		],
+		"name": "setMinimartItem",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "index",
+				"type": "uint256"
+			}
+		],
+		"name": "unListItem",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "approvedCollection",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "fetchData",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "address",
+						"name": "contractAddress",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "tokenId",
+						"type": "uint256"
+					},
+					{
+						"internalType": "address",
+						"name": "owner",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "itemPrice",
+						"type": "uint256"
+					}
+				],
+				"internalType": "struct returnData[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "guacToken",
+		"outputs": [
+			{
+				"internalType": "contract IGUAC",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "minimartItemContract",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "minimartItemPrice",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "minimartItemTokenId",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "nftToTransfer",
+		"outputs": [
+			{
+				"internalType": "contract INft",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "num",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "returnApprovedContracts",
+		"outputs": [
+			{
+				"internalType": "address[]",
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalListed",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
