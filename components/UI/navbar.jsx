f4742e88c7706f3b37d81eb87d02a37f25a47df4
Risav03 <needleweed03@gmail.com>
2024-02-06 09:34:34 +0530

    added raffle button

--- components/UI/navbar.jsx
+++ components/UI/navbar.jsx
@@ -7,8 +7,12 @@ export default function Navbar(){
     const router = useRouter();
 
     return(
-        <div className="flex flex-row justify-between w-[80%] mx-auto mt-2">
-           <button onClick={()=>{router.push("/minimart")}} className="px-4 py-1 rounded-full border-2 border-black bg-yellow-400 text-black">Minimart</button>
+        <div className="flex px-24 py-5 gap-20 mb-10">
+
+        <div className="flex flex-row justify-between w-fit gap-5  mt-2">
+           <button onClick={()=>{router.push("/minimart")}} className="px-4 py-1 hover:bg-yellow-500 rounded-full border-2 border-black bg-yellow-400 text-black">Minimart</button>
+           <button onClick={()=>{router.push("/")}} className="px-4 py-1 hover:bg-yellow-500 rounded-full border-2 border-black bg-yellow-400 text-black">Raffle</button>
+        </div>
             <WalletConnectButton/>
         </div>
     )
